396fbacd4193a80a04b1cc02add6c0aadd2df9c3
smathew32 <smathew32@myseneca.ca>
2024-03-11 14:26:00 -0400

    added more information user needs to enter such as tenant contact information, email notices

--- form.js
+++ form.js
@@ -23,6 +23,12 @@ app.get('/', (req, res) => {
             <label for="landlord_name">Landlord's Legal Name:</label>
             <input type="text" id="landlord_name" name="landlord_name"><br>
             
+            <h3>Rentee Information</h3>
+            <label for="first_name">First Name:</label>
+            <input type="text" id="first_name" name="first_name"><br>
+            <label for="last_name">Last Name:</label>
+            <input type="text" id="last_name" name="last_name"><br>
+            
             <h3>Rental Unit Information</h3>
             <label for="retail_unit">Retail Unit:</label>
             <input type="text" id="retail_unit" name="retail_unit"><br>
@@ -36,8 +42,53 @@ app.get('/', (req, res) => {
             <input type="text" id="province" name="province"><br>
             <label for="postal_code">Postal Code:</label>
             <input type="text" id="postal_code" name="postal_code"><br>
-            <button type="submit">Submit</button>
+
+            <h3>Tenant Contact Information</h3>
+            <label for="tenant_retail_unit">Retail Unit:</label>
+            <input type="text" id="tenant_retail_unit" name="tenant_retail_unit"><br>
+            <label for="tenant_street_number">Street Number:</label>
+            <input type="text" id="tenant_street_number" name="tenant_street_number"><br>
+            <label for="tenant_street_name">Street Name:</label>
+            <input type="text" id="tenant_street_name" name="tenant_street_name"><br>
+            <label for="tenant_PO_box">PO Box:</label>
+            <input type="text" id="tenant_PO_box" name="tenant_PO_box"><br>
+            <label for="tenant_city_town">City/Town:</label>
+            <input type="text" id="tenant_city_town" name="tenant_city_town"><br>
+            <label for="tenant_province">Province:</label>
+            <input type="text" id="tenant_province" name="tenant_province"><br>
+            <label for="tenant_postal_code">Postal Code:</label>
+            <input type="text" id="tenant_postal_code" name="tenant_postal_code"><br>
+            
+            <h3>Email Notices</h3>
+        <p>Both the landlord and tenant agree to receive notices and documents by email, where allowed by the Landlord and Tenant Board’s Rules of Procedure.</p>
+        <label for="email_agreement">Agreement:</label>
+        <input type="checkbox" id="email_agreement" name="email_agreement" value="yes">
+        <label for="email_agreement">Yes</label>
+        <input type="checkbox" id="no_email_agreement" name="email_agreement" value="no">
+        <label for="email_agreement">No</label>
+        
+        <div id="email_address_container" style="display: none;">
+            <label for="email_address">Email Address:</label>
+            <input type="text" id="email_address" name="email_address">
+        </div>
+        
+        <button type="submit">Submit</button>
+
         </form>
+
+        <script>
+        const emailAgreementCheckbox = document.getElementById('email_agreement');
+        const emailAddressContainer = document.getElementById('email_address_container');
+
+        emailAgreementCheckbox.addEventListener('change', function() {
+            if (this.checked) {
+                emailAddressContainer.style.display = 'block';
+            } else {
+                emailAddressContainer.style.display = 'none';
+            }
+        });
+
+        </script>
     </body>
     </html>
   `);
@@ -52,6 +103,8 @@ app.post('/', (req, res) => {
   const cityTown = req.body.city_town;
   const province = req.body.province;
   const postalCode = req.body.postal_code;
+
+
   res.send(`
     <h2>Tenant Information Received</h2>
     <p>Landlord's Legal Name: ${landlordName}</p>
